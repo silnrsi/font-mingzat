82e6ee8e2e7209406a2b6a074a6dacd45e1b13f0
Victor Gaultney <victor_gaultney@sil.org>
2019-03-19 20:31:55 +0000

    Initial files based on original Mingzat project

---
+++ source/graphite/comp.bat
@@ -0,0 +1,4 @@
+grcompiler -d main.gdl "C:\0NRSI\svn\lepcha\Source files\VOLT\Mingzat-hackos.ttf" Mingzat-gdl.ttf
+rem c:\progra~1\Graphite\grcompiler /d %1.gdl %2.ttf
+pause
+
---
+++ source/graphite/gdl.bat
@@ -0,0 +1,1 @@
+make_gdl -a ..\..\FontLab\Mingzat-anchors.xml -z 2 C:\0NRSI\svn\lepcha\Source~1\VOLT\Mingzat-hackos.ttf glyph.gdh
